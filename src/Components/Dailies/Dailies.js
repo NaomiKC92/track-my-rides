c66195aa061411f3dae417f7f319206739518d65
Naomi Campos <naomicampos@nkcampos.local>
2020-03-27 13:36:07 -0600

    Add section for daily information cards

---
+++ src/Components/Dailies/Dailies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dailies = ({rides, hours, moneyMade, moneySpent}) => {
+  return (
+    <div>
+      <h2>Daily Records</h2>
+    </div>
+  )
+}
+
+export default Dailies
