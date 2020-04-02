5a60098a75c1da3f07b767fe6fc47d709e7640be
Naomi Campos <naomicampos@nkcampos.local>
2020-04-02 10:50:02 -0600

    Add containers for form component and daily component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Dailies from '../Dailies/Dailies'
+import Dailies from '../../Containers/DailyInfoContainer/DailyInfoContainer.js'
 import DailyInputForm from '../DailyInputForm/DailyInputForm'
 
 class App extends Component {
